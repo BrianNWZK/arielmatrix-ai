9bae5d7ef63a3bf5e20e498df6153df11bd9469b
BrianNWZK <brianwaezike@gmail.com>
2025-08-09 09:41:23 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/*.html'],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/*.html'
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
